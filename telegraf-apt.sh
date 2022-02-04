e655e792beee0b05d76f8ed101475fc12db109c4
x70b1 <x70b1@users.noreply.github.com>
2022-02-04 01:20:14 +0100

    telegraf-apt.sh: cache LTS info

--- telegraf-apt.sh
+++ telegraf-apt.sh
@@ -2,17 +2,26 @@
 
 release_version=$(cat /etc/debian_version)
 release_codename=$(lsb_release -s -c | sed 's/^[a-z]/\U&/g')
-release_status=$(curl -sf https://wiki.debian.org/LTS | grep "$(echo "Debian $release_version" | cut -d '.' -f 1)")
-release_support=2
+release_ltsfile="/tmp/telegraf-apt_LTS.html"
 
-updates_regular=$(apt-get -qq -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | grep ^Inst | grep -c -v Security)
-updates_security=$(apt-get -qq -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | grep ^Inst | grep -c Security)
+
+if [ ! -f $release_ltsfile ] || [ "$(find "$release_ltsfile" -mtime +1 -print)" ]; then
+    curl -sf -o $release_ltsfile https://wiki.debian.org/LTS
+fi
+
+release_status=$(cat $release_ltsfile | grep "$(echo "Debian $release_version" | cut -d '.' -f 1)")
 
 if echo "$release_status" | grep -q "#98fb98"; then
     release_support=0
 elif echo "$release_status" | grep -q "#FCED77"; then
     release_support=1
+else
+    release_support=2
 fi
+
+
+updates_regular=$(apt-get -qq -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | grep ^Inst | grep -c -v Security)
+updates_security=$(apt-get -qq -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | grep ^Inst | grep -c Secursity)
 
 if [ "$updates_security" -gt 0 ] && [ "$updates_regular" -gt 0 ]; then
     updates_severity=3
