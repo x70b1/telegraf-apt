618e671885d40ac4d3e54fa973365c8e2cf2113e
x70b1 <x70b1@users.noreply.github.com>
2022-03-03 13:34:42 +0100

    telegraf-apt.sh: remove LTS info caching

--- telegraf-apt.sh
+++ telegraf-apt.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-apt_ltsfile="/tmp/telegraf-apt.ltsinfo"
-
 case "$1" in
     --update)
         kill -USR1 "$(pgrep -f -o "$0")"
@@ -12,18 +10,13 @@ case "$1" in
 
         while true; do
             release_version=$(cat /etc/debian_version)
-            release_codename=$(lsb_release -s -c | sed 's/^[a-z]/\U&/g')
+            release_codename=$(. /etc/os-release; echo "$VERSION_CODENAME" | sed 's/^[a-z]/\U&/g')
 
-            if [ ! -f $apt_ltsfile ] || [ "$(find $apt_ltsfile -mtime +1 -print)" ]; then
-                curl -sf -o $apt_ltsfile https://wiki.debian.org/LTS
-            fi
+            release_ltsinfo=$(curl -sf https://wiki.debian.org/LTS | grep "Debian $(echo "$release_version" | cut -d '.' -f 1)")
 
-
-            release_status=$(grep "$(echo "Debian $release_version" | cut -d '.' -f 1)" $apt_ltsfile)
-
-            if echo "$release_status" | grep -q "#98fb98"; then
+            if echo "$release_ltsinfo" | grep -q "#98fb98"; then
                 release_support=0
-            elif echo "$release_status" | grep -q "#FCED77"; then
+            elif echo "$release_ltsinfo" | grep -q "#FCED77"; then
                 release_support=1
             else
                 release_support=2
